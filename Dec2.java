5116d06f885790ea3f39006f4d8f1b981b16271d
Karthik P <karthiksar15@gmail.com>
2024-12-02 07:09:32 +0530

    square non descending

---
+++ Dec2.java
@@ -0,0 +1,39 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Dec2 {
+
+    /**
+     * The main method of the Dec2 class contains a sample scenario for
+     * demonstrating the sortedSquares method. It creates an array of integers
+     * and prints the array of squares of the integers in sorted order to the
+     * console.
+     */
+    public static void main(String[] args) {
+
+        int[] nums = { -4, -1, 0, 3, 10 };
+        int[] result = sortedSquares(nums);
+        IntStream.of(result).forEach(System.out::print);
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int i = 0;
+        int j = nums.length - 1;
+        int index = nums.length - 1;
+        while (i != j) {
+            int prd1 = nums[i] * nums[i];
+            int prd2 = nums[j] * nums[j];
+            if (prd1 > prd2) {
+                result[index] = prd1;
+                i++;
+            } else {
+                result[index] = prd2;
+                j--;
+            }
+            index--;
+        }
+        return result;
+    }
+
+}
