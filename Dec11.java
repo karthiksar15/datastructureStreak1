aec68da838f8ba547ec1452963a7a9a4941b01af
Karthik P <karthiksar15@gmail.com>
2024-12-12 00:33:06 +0530

    number of wins and losses

--- Dec11.java
+++ Dec11.java
@@ -1,6 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Dec11 {
 
     public static void main(String[] args) {
+        int[][] matches = { { 1, 3 }, { 2, 3 }, { 3, 6 }, { 5, 6 }, { 5, 7 }, { 4, 5 }, { 4, 8 }, { 4, 9 }, { 10, 4 },
+                { 10, 9 } };
+        System.out.println(winsAndLoses(matches));
+    }
+
+    public static List<List<Integer>> winsAndLoses(int[][] matches) {
+        Set<Integer> zeroLoss = new HashSet<>(), oneLoss = new HashSet<>(),
+                moreLosses = new HashSet<>();
+
+        for (int[] match : matches) {
+            int winner = match[0], loser = match[1];
+            // Add winner.
+            if (!oneLoss.contains(winner) && !moreLosses.contains(winner)) {
+                zeroLoss.add(winner);
+            }
+            // Add or move loser.
+            if (zeroLoss.contains(loser)) {
+                zeroLoss.remove(loser);
+                oneLoss.add(loser);
+            } else if (oneLoss.contains(loser)) {
+                oneLoss.remove(loser);
+                moreLosses.add(loser);
+            } else if (moreLosses.contains(loser)) {
+                continue;
+            } else {
+                oneLoss.add(loser);
+            }
+        }
+
+        List<List<Integer>> answer = Arrays.asList(new ArrayList<>(), new ArrayList<>());
+        answer.get(0).addAll(zeroLoss);
+        answer.get(1).addAll(oneLoss);
+        Collections.sort(answer.get(0));
+        Collections.sort(answer.get(1));
+
+        return answer;
 
     }
 
