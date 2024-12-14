0629489031cc4bfd5dc79d39286081312f444b1c
Karthik P <karthiksar15@gmail.com>
2024-12-14 22:38:58 +0530

    longest non duplicate number

--- Dec12.java
+++ Dec12.java
@@ -1,3 +1,34 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class Dec12 {
+
+    public static void main(String[] args) {
+        int[] nums = { 5, 7, 3, 9, 4, 9, 8, 3, 1 };
+        System.out.println(largetNumber(nums));
+    }
+
+    public static int largetNumber(int[] nums) {
+
+        // solution:1
+        var map = new HashMap<Integer, Integer>();
+        int max = -1;
+        for (int num : nums) {
+            map.computeIfAbsent(num, k -> 0);
+            map.put(num, map.get(num) + 1);
+        }
+        for (Entry<Integer, Integer> set : map.entrySet()) {
+            if (set.getKey() > max && set.getValue() == 1) {
+                max = Math.max(max, set.getKey());
+            }
+        }
+
+        return max;
+
+    }
 
 }
