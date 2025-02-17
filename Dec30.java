8aab5034286b8e0c2f126efe124dd9032597fe08
Karthik P <karthiksar15@gmail.com>
2025-02-17 22:55:59 +0530

    Update Dec30.java

--- Dec30.java
+++ Dec30.java
@@ -1,1 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
 
+public class Dec30 {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int i = 0, j = 0; j < s.length(); j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            count = Math.max(count, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return count;
+
+    }
+}
