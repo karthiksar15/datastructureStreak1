1884512e34f6d674a2f7ef5fb1524d142c299880
Karthik P <karthiksar15@gmail.com>
2025-02-18 22:35:55 +0530

    Update Dec31.java

--- Dec31.java
+++ Dec31.java
@@ -1,1 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Dec31 {
+
+    public static void main(String[] args) {
+        String jewels = "aA";
+        String stones = "aAAbbbb";
+        System.out.println(numJewelsInStones(jewels, stones));
+    }
+
+    public static int numJewelsInStones(String jewels, String stones) {
+        Set<Character> set = new HashSet<>();
+        int count = 0;
+        for (char c : jewels.toCharArray()) {
+            set.add(c);
+        }
+        for (char c : stones.toCharArray()) {
+            if (set.contains(c))
+                count++;
+        }
+
+        return count;
+    }
+
+}
 
