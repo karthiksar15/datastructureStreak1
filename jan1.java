62bd9da1fd903e41c1ce6af31569dfd3a7b4899b
Karthik P <karthiksar15@gmail.com>
2025-02-20 22:40:26 +0530

    Update jan1.java

--- jan1.java
+++ jan1.java
@@ -1,1 +1,28 @@
 
+public class Jan1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class Node {
+    public int val;
+    public Node next;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    public void addNode(Node prevNode, Node newNode) {
+        newNode.next = prevNode.next;
+        prevNode.next = newNode;
+    }
+
+    public void deleteNode(Node prevNode) {
+        prevNode.next = prevNode.next.next;
+    }
+
+}
