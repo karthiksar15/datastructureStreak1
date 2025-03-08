5fc74f56a7986e4609422aea3ce10eecfecb1b6d
Karthik P <karthiksar15@gmail.com>
2025-03-08 08:20:06 +0530

    Update Jan6.java

--- Jan6.java
+++ Jan6.java
@@ -1,1 +1,19 @@
+  StringBuilder result = new StringBuilder();
+        Stack<String> stack = new Stack();
+        String[] paths = path.split("/");
+        for (String str : paths) {
+            if (str.equalsIgnoreCase("..") && (!stack.isEmpty())) {
+                stack.pop();
+            } else if (!(str.equalsIgnoreCase("..") ||
+                    str.equalsIgnoreCase(".") ||
+                    str.length() == 0)) {
+                stack.push("/" + str);
+            }
+        }
+        for (String str : stack) {
+            result.append(str);
+        }
+        if (result.length() == 0)
+            return result.append("/").toString();
+        return result.toString();
 
