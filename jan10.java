4ea0fda16e13bbfd259c97b9c3c27dd15477d2b4
Karthik P <karthiksar15@gmail.com>
2025-03-19 07:20:53 +0530

    Update jan10.java

--- jan10.java
+++ jan10.java
@@ -7,7 +7,7 @@ public class Jan12 {
     public static void main(String[] args) {
 
         StockSpanner sp = new StockSpanner();
-        int[] stocks = { 100, 80, 60, 70, 60, 75, 85 };
+        int[] stocks = { 90, 21, 21, 68 };
         for (int num : stocks) {
             int result = sp.next(num);
             System.out.println(result);
@@ -19,20 +19,32 @@ public class Jan12 {
 
 class StockSpanner {
 
-    Stack<int[]> stack = new Stack<>();
+    Stack<Integer> stack = new Stack<>();
 
     public StockSpanner() {
     }
 
     public int next(int price) {
-        int ans = 1;
-        while (!stack.isEmpty() && stack.peek()[0] <= price) {
-            ans += stack.pop()[1];
+
+        // recomend
+        // int ans = 1;
+        // while (!stack.isEmpty() && stack.peek()[0] <= price) {
+        // ans += stack.pop()[1];
+        // }
+
+        // stack.push(new int[] { price, ans });
+        // return ans;
+
+        // my soln
+        int count = 1;
+        if (!stack.isEmpty() && stack.peek() < price) {
+            for (int i = stack.size() - 1; i >= 0; i--) {
+                if (stack.get(i) > price)
+                    break;
+                count++;
+            }
         }
-
-        stack.push(new int[] { price, ans });
-        return ans;
-
+        stack.push(price);
+        return count;
     }
 }
-
