4cada4a7bd099195bd0c9e1bda40d03483214530
Karthik P <karthiksar15@gmail.com>
2025-12-07 12:14:50 +0530

    Update Feb-19.java

--- Feb-19.java
+++ Feb-19.java
@@ -1,1 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Feb20 {
+
+    public static void main(String[] args) {
+        int n = 6;
+        List<List<Integer>> edges = List.of(List.of(0, 1), List.of(0, 2), List.of(2, 5), List.of(3, 4), List.of(4, 2));
+        Feb20 feb = new Feb20();
+        System.out.println("set-->" + feb.findSmallestSetOfVertices(n, edges));
+    }
+
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        boolean[] indgree = new boolean[n];
+        List<Integer> setVertices = new ArrayList<>();
+        for (List<Integer> vertex : edges) {
+            if (!indgree[vertex.get(1)]) {
+                indgree[vertex.get(1)] = true;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (!indgree[i]) {
+                setVertices.add(i);
+            }
+        }
+        return setVertices;
+    }
+
+}
 
