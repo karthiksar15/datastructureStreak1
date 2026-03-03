b0e75916e40c220e9d63f5f502fc1c5e150b3940
Karthik P <karthiksar15@gmail.com>
2026-03-03 23:38:43 +0530

    Create Mar0326.java

---
+++ Mar0326.java
@@ -0,0 +1,16 @@
+public class Mar0226 {
+    public static void main(String[] args) {
+        Mar0226 mar = new Mar0226();
+        int[] nums = { 1, 1, 2, 3, 4 };
+        int val = 1;
+        System.out.println("remove element--->" + mar.removeElement(nums, val));
+    }
+
+    public int removeElement(int[] nums, int val) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+    }
+
+}
