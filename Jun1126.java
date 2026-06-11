ca21c7aa4563abf3da6baf0f996dd80ade39a060
Karthik P <karthiksar15@gmail.com>
2026-06-11 23:56:14 +0530

    Jun11 commits

---
+++ Jun1126.java
@@ -0,0 +1,33 @@
+public class Jun1126 {
+
+    public static void main(String[] args) {
+        Jun1126 jun = new Jun1126();
+        int[] nums = { 1, 2, 3, 4 };
+        System.out.println("canPartition-->" + jun.canPartition(nums));
+    }
+
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int num : nums)
+            sum += num;
+        if (sum % 2 != 0)
+            return false;
+        int target = sum / 2;
+        boolean[][] dp = new boolean[n + 1][target + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][target];
+    }
+
+}
