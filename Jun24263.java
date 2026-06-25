dcc271f592942f2401a4a2793c091bca280d5ccb
Karthik P <karthiksar15@gmail.com>
2026-06-25 22:42:08 +0530

    Jun2526 commits

--- Jun24263.java
+++ Jun24263.java
@@ -11,7 +11,18 @@ public class Jun24263 {
     }
 
     public boolean wordBreak(String s, List<String> wordDict) {
-
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[s.length()] = true;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (String str : wordDict) {
+                if (i + str.length() <= s.length() && s.substring(i, i + str.length()).equals(str)) {
+                    dp[i] = dp[i + str.length()];
+                }
+                if (dp[i])
+                    break;
+            }
+        }
+        return dp[0];
     }
 
 }
---
+++ Jun2526.java
@@ -0,0 +1,12 @@
+public class Jun2526 {
+
+    public static void main(String[] args) {
+        int[] nums = { 9, 1, 4, 2, 3, 3, 7 };
+        Jun2526 jun = new Jun2526();
+        System.out.println("length--->" + jun.lengthOfLIS(nums));
+    }
+
+    public int lengthOfLIS(int[] nums) {
+    }
+
+}
