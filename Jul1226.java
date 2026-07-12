0a1fdd218833d0ef99425246aaa3a681962fd73e
Karthik P <karthiksar15@gmail.com>
2026-07-12 20:17:26 +0530

    Jul12 commits

---
+++ Jul1226.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Jul1226 {
+
+    public static void main(String[] args) {
+        Jul1226 jul = new Jul1226();
+        int[][] intervals = { { 1, 3 }, { 4, 6 } };
+        int[] newInterval = { 2, 5 };
+        System.out.println(Arrays.toString(jul.insert(intervals, newInterval)));
+    }
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+
+    }
+
+}
