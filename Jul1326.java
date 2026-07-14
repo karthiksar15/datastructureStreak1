0317ad262a4aba0e6129afc5158fed919d47777e
Karthik P <karthiksar15@gmail.com>
2026-07-14 22:44:57 +0530

    14JUL commits

--- Jul1326.java
+++ Jul1326.java
@@ -14,7 +14,14 @@ public class Jul1326 {
         dp[1][0] = nums[1];
         dp[1][1] = nums[0];
 
-        for(int i=)
+        for (int i = 2; i < n - 1; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 2][0] + nums[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][1] + nums[i]);
+        }
+        int last = n - 1;
+        dp[last][0] = Math.max(dp[last - 1][0], dp[last - 2][0] + nums[last]);
+        dp[last][1] = dp[last - 1][1];
+        return Math.max(dp[n - 1][0], dp[n - 1][1]);
     }
 
 }
