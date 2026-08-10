6669ffeb57d4d73b88bfe93e52ba65a32c86f813
Karthik P <karthiksar15@gmail.com>
2026-08-10 23:05:53 +0530

    Aug10 commits

---
+++ Aug1026.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Aug1026 {
+
+    public static void main(String[] args) {
+        int n = 2;
+        int[][] meetings = { { 1, 10 }, { 2, 10 }, { 3, 10 }, { 4, 10 } };
+        Aug1026 aug = new Aug1026();
+        System.out.println("mostBooked--->" + aug.mostBooked(n, meetings));
+    }
+
+    public int mostBooked(int n, int[][] meetings) {
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> Integer.compare(a[0], b[0]) == 0 ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            pq.offer(new int[] { 0, i });
+        }
+        int[] count = new int[n];
+        for (int[] meeting : meetings) {
+            int start=meeting[0];
+            int end=meeting[1];
+            if(!pq.isEmpty() && pq.peek()[1])
+        }
+    }
+
+}
