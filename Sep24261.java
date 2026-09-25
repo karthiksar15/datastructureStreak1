2cb810aab3d133f442f4e3049c1cdd8df3d8f021
Karthik P <karthiksar15@gmail.com>
2026-09-25 22:20:44 +0530

    Sep2526 commits

--- Sep24261.java
+++ Sep24261.java
@@ -1,7 +1,25 @@
 public class Sep24261 {
 
     public static void main(String[] args) {
+        String text1 = "cat", text2 = "crabt";
+        Sep24261 sep = new Sep24261();
+        System.out.println("longest--->" + sep.longestCommonSubsequence(text1, text2));
+    }
 
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 
 }
